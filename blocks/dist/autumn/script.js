1388b38e6e49bf35f4b98adc22064ee5b9f88e0f
Junrong <junrongh94@outlook.com>
2021-01-17 23:27:37 +0800

    wip: fix blink image loading

--- blocks/dist/autumn/script.js
+++ blocks/dist/autumn/script.js
@@ -1,14 +1,37 @@
 let x = 0;
-let x_new = 0;
-let x_offset = 0;
+let xNew = 0;
+let xOffset = 0;
 
-let autumnBanner = document.getElementById('autumn_banner');
-let images = autumnBanner.children;
+const autumnBanner = document.getElementById('autumn_banner');
 
-let window_width = document.documentElement.clientWidth;
-let step = window_width / 2 / 5;
+autumnBanner.innerHTML = `
+    <div class="image"><img width="3000" height="250" src="dist/autumn/bg.png"> </div>
+    <div class="image"><img width="1800" height="165" src="dist/autumn/girl1.png"> </div>
+    <div class="image"><img width="3000" height="250" src="dist/autumn/grassland.png"> </div>
+    <div class="image"><img width="1800" height="160" src="dist/autumn/mushroom.png"> </div>
+    <div class="image"><img width="1800" height="165" src="dist/autumn/spirit.png"> </div>
+    <div class="image"><img width="1950" height="178" src="dist/autumn/leaf.png"> </div>
+`;
 
-let data_images = [
+const blink0 = autumnBanner.children[1];
+const blink1 = document.createElement('div');
+blink1.className = 'image';
+blink1.innerHTML = `<img width="1800" height="165" src="dist/autumn/girl2.png">`
+const blink2 = document.createElement('div');
+blink2.className = 'image';
+blink2.innerHTML = `<img width="1800" height="165" src="dist/autumn/girl3.png">`
+const blink3 = document.createElement('div');
+blink3.className = 'image';
+blink3.innerHTML = `<img width="1800" height="165" src="dist/autumn/girl4.png">`
+
+const blinkNodes = [blink0, blink1, blink2, blink3];
+
+const images = autumnBanner.children;
+
+const width = document.documentElement.clientWidth;
+const step = width / 2 / 5;
+
+const dataImages = [
     { x: 0, b: 4 },
     { x: 0, b: 0 },
     { x: 0, b: 1 },
@@ -17,33 +40,31 @@ let data_images = [
     { x: 0, b: 6 },
 ]
 
-let init = () => {
-    for (const key in images) {
-        if (images.hasOwnProperty(key)) {
-            const element = images[key];
-            const element_data = data_images[key];
-            element.children[0].style = 'transition: .2s linear; transform: translate(' + element_data.x + 'px); filter: blur(' + element_data.b + 'px);';
+function init() {
+    for (let i = 0; i < 6; i++) {
+        if (images.hasOwnProperty(i)) {
+            const element = images[i];
+            const elementData = dataImages[i];
+            element.children[0].style = 'transition: .2s linear; transform: translate(' + elementData.x + 'px); filter: blur(' + elementData.b + 'px);';
         }
     }
 }
 
 autumnBanner.addEventListener('mouseover', (e) => {
     x = e.clientX;
-    // console.log(x);
 });
+
 autumnBanner.addEventListener("mousemove", (e) => {
-    x_new = e.clientX;
-    // console.log(x_new);
-    x_offset = x - x_new;
-    // console.log(x_offset);
-    for (const key in images) {
-        if (images.hasOwnProperty(key)) {
-            let level = (6 - parseInt(key)) * 10;
-            const element = images[key];
-            const element_data = data_images[key];
-            let b_new = Math.abs(element_data.b + (x_offset / step));
-            let l_new = 0 - (x_offset / level);
-            element.children[0].style = 'transform: translate(' + l_new + 'px); filter: blur(' + b_new + 'px);';
+    xNew = e.clientX;
+    xOffset = x - xNew;
+    for (const i in images) {
+        if (images.hasOwnProperty(i)) {
+            const level = (6 - parseInt(i)) * 10;
+            const element = images[i];
+            const elementData = dataImages[i];
+            const bNew = Math.abs(elementData.b + (xOffset / step));
+            const lNew = 0 - (xOffset / level);
+            element.children[0].style = 'transform: translate(' + lNew + 'px); filter: blur(' + bNew + 'px);';
         }
     }
 
@@ -54,19 +75,23 @@ autumnBanner.addEventListener("mouseout", (e) => {
     init();
 });
 
-blink_index = 0;
-timeout = 4000;
-let blink = () => {
+let blinkIndex = 0;
+let timeout = 4000;
 
-    if (blink_index == 4) {
-        blink_index = 1;
+const blink = () => {
+    if (blinkIndex === 4) {
+        console.log(blinkIndex);
+        blinkIndex = 1;
         timeout = 4000;
     } else {
-        blink_index += 1;
+        blinkIndex += 1;
         timeout = 30;
     }
 
-    images[1].children[0].src = 'dist/autumn/girl' + blink_index + '.png';
+    console.log(blinkIndex);
+    images[1].children[0] = blinkNodes[blinkIndex - 1].children;
+    // autumnBanner.children[1] = blinkNodes[blinkIndex]
+    // images[1].children[0].src = 'dist/autumn/girl' + blinkIndex + '.png';
     setTimeout(() => {
         blink();
     }, timeout);
